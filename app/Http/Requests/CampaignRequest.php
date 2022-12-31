9c0ca0fe37a0a055a723ccea5fcd2e5ed0c9e3b5
lee phpc <13032774-lee.phpc@users.noreply.gitlab.com>
2022-12-31 02:56:45 +0000

    Update CampaignRequest.php

--- app/Http/Requests/CampaignRequest.php
+++ app/Http/Requests/CampaignRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use App\Enums\CampaignType;
-use App\Enums\ReceiverStatus;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Enum;
